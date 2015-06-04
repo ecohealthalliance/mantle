d8776cdc91d0c61e7dd7ffc45c767247fadabffd
Nathan Breit <nabreit@gmail.com>
2015-06-04 04:44:29 -0400

    This reproduces the issue where template helpers are not passed to partials

--- packages/splash-page/package.js
+++ packages/splash-page/package.js
@@ -8,8 +8,11 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.use('templating');
+  api.use('coffeescript');
   api.use('mquandalle:jade@0.4.1');
   api.addFiles('splash_page.jade', 'client');
+  api.addFiles('splash_page.coffee', 'client');
+  
 });
 
 Package.onTest(function(api) {
---
+++ packages/splash-page/splash_page.coffee
@@ -0,0 +1,1 @@
+Template.splashPage.testVar = -> "testVar"
