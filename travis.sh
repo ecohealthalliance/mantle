c247e499927e2d0e139174169c9bcddd2ff7e2ae
Freddie Rosario <rosario@ecohealthalliance.org>
2015-07-20 09:56:21 -0400

    Saucelabs

--- travis.sh
+++ travis.sh
@@ -5,10 +5,17 @@ type spacejam || npm install -g spacejam
 spacejam test-packages packages/* || touch FAILURE
 
 # Run cucumber tests ###########################################################
+echo "Run cucumber tests against phantomjs ************************************"
 VELOCITY_CI=1 CHIMP_OPTIONS="" meteor --test || touch FAILURE
 cat .meteor/local/log/cucumber.log
 
-# Trigger testing in saucelabs #################################################
-npm install -g mocha grunt grunt-init grunt-cli
-git clone https://github.com/saucelabs/grunt-init-sauce.git ~/.grunt-init/sauce
+# Saucelabs #################################################
+#echo "Run cucumber tests on saucelabs *****************************************"
+#HUB_HOST=ondemand.saucelabs.com
+#HUB_PORT=4444
+#HUB_USER=$SAUCE_USERNAME
+#HUB_KEY=$SAUCE_ACCESS_KEY
+#HUB_PLATFORM='Windows 7'
+#HUB_VERSION=35
+#VELOCITY_CI=1 CHIMP_OPTIONS="" meteor --test
 
