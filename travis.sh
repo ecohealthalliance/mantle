be5b78b605eba926efa47fca055a0a6b79e0c49a
Freddie Rosario <rosario@ecohealthalliance.org>
2015-08-14 10:53:02 -0400

    Testing CircleCI

--- travis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-type spacejam || npm install -g spacejam
-spacejam test-packages packages/* || touch FAILURE
-VELOCITY_CI=1 CHIMP_OPTIONS="" CUCUMBER_TAGS=~@chrome meteor --test || touch FAILURE
-cat .meteor/local/log/cucumber.log
-
