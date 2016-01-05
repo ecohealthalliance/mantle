4a5ac63cd0213a70932ddf9b9e6da8e59fb2698d
Daniel Sullivan <danielalexandersullivan@gmail.com>
2016-01-05 16:01:48 -0500

    Remove models for now

--- packages/models/package.js
+++ packages/models/package.js
@@ -15,13 +15,6 @@ Package.onUse(function(api) {
   api.use('mongo');
   api.use('cfs:standard-packages');
   api.use('cfs:gridfs');
-  api.addFiles('user_profiles.coffee', ['client', 'server']);
-  api.addFiles('organizations.coffee', ['client', 'server']);
-  api.addFiles('raw_files.coffee', ['client', 'server']);
-  api.addFiles('datasets.coffee', ['client', 'server']);
-  api.export(['UserProfile', 'UserProfiles'], ['client', 'server']);
-  api.export(['Organization', 'Organizations'], ['client', 'server']);
-  api.export(['Dataset', 'Datasets'], ['client', 'server']);
 });
 
 Package.onTest(function(api) {
@@ -30,7 +23,4 @@ Package.onTest(function(api) {
   api.use('mantle:models');
   api.use('practicalmeteor:munit');
   api.use('test-helpers');
-  api.addFiles('tests/server/user_profiles_test.coffee', 'server');
-  api.addFiles('tests/server/organizations_test.coffee', 'server');
-  api.addFiles('tests/server/datasets_test.coffee', 'server');
 });
