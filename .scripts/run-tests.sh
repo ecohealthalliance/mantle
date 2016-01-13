30eab707cddf5942debf9b9a15f55a7600f6eeec
Daniel Sullivan <danielalexandersullivan@gmail.com>
2016-01-13 14:34:51 -0500

    Manually set exit codes in test running script

--- .scripts/run-tests.sh
+++ .scripts/run-tests.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 MONGO_URL=mongodb://localhost:27017/chimp_db meteor --port 3100 &
-CUCUMBER_TAIL=1 ./node_modules/.bin/chimp --tags=${TAGS} --ddp=http://localhost:3100 --path=tests/cucumber/features/ --coffee=true --chai=true --sync=false
+tail -f testoutput.txt &
+CUCUMBER_TAIL=1 ./node_modules/.bin/chimp --tags=${TAGS} --ddp=http://localhost:3100 --path=tests/cucumber/features/ --coffee=true --chai=true --sync=false > testoutput.txt
 kill `lsof -t -i:3100`
+if grep -q "failed steps" testoutput.txt
+then
+  echo "Tests Failed"
+  exit 1  
+fi
+echo "Tests Passed"
+exit 0
