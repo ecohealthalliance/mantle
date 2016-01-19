a01ccedf6b581c747534a0323d8c2c872aaed23a
Daniel Sullivan <danielalexandersullivan@gmail.com>
2016-01-19 14:00:56 -0500

    Run tests in synchronous mode (default in chimp)

--- .scripts/run-tests.sh
+++ .scripts/run-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 MONGO_URL=mongodb://localhost:27017/chimp_db meteor --port 3100 &
-CUCUMBER_TAIL=1 ./node_modules/.bin/chimp --tags=${TAGS} --ddp=http://localhost:3100 --path=tests/cucumber/features/ --coffee=true --chai=true --sync=false
+CUCUMBER_TAIL=1 ./node_modules/.bin/chimp --tags=${TAGS} --ddp=http://localhost:3100 --path=tests/cucumber/features/ --coffee=true --chai=true
 kill `lsof -t -i:3100`
